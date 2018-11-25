b5cd5762fc525e62f7defee9e0574715fafd2d74
Vivek Vellaiyappan <techengineervivek@gmail.com>
2018-11-24 19:30:35 -0500

    default url mapping done. note that there is no () in @RequestMapping

--- Code/src/main/java/corp/vivek/tech/welcome/WelcomeController.java
+++ Code/src/main/java/corp/vivek/tech/welcome/WelcomeController.java
@@ -19,29 +19,41 @@ import org.springframework.web.bind.annotation.RestController;
 public class WelcomeController {
 
     /**
+     * Default url mapping
+     * Note that there is no () in @RequestMapping
+     */
+    @RequestMapping
+    public String welcomeMsg() {
+        return welcome();
+    }
+
+    /**
      * Welcome class to display a welcome message
      *
      * @return welcome string message
      */
-     // @RestController to make the class use Spring MVC
-     // @RequestMapping helps to map the url args using http methods (get, post, put, update, delete) to get the class's response and display in the web
+    // @RestController to make the class use Spring MVC
+    // @RequestMapping helps to map the url args using http methods (get, post, put, update, delete) to get the class's response and display in the web
     @RequestMapping("/welcome")
     public String welcome() {
-        return "Hello Spring Boot World! Looking forward to do amazing things with you! yours, keviv";
+        return "Hello Spring Boot World! Looking forward to do amazing things with you! yours, Vivek";
     }
 }
 
 /*
 Output:
 >
-In web appln -> navigated to http://localhost:8080/welcome
-Hello Spring Boot World! Looking forward to do amazing things with you! yours, keviv
+In web appln -> navigated to http://localhost:8080
+Hello Spring Boot World! Looking forward to do amazing things with you! yours, Vivek
 <
 
 >
 In console log
-2018-11-23 18:14:45.457  INFO 8456 --- [nio-8080-exec-1] o.a.c.c.C.[Tomcat].[localhost].[/]       : Initializing Spring FrameworkServlet 'dispatcherServlet'
-2018-11-23 18:14:45.457  INFO 8456 --- [nio-8080-exec-1] o.s.web.servlet.DispatcherServlet        : FrameworkServlet 'dispatcherServlet': initialization started
-2018-11-23 18:14:45.488  INFO 8456 --- [nio-8080-exec-1] o.s.web.servlet.DispatcherServlet        : FrameworkServlet 'dispatcherServlet': initialization completed in 31 ms
+2018-11-24 19:27:55.145  INFO 14196 --- [           main] o.s.j.e.a.AnnotationMBeanExporter        : Registering beans for JMX exposure on startup
+2018-11-24 19:27:55.198  INFO 14196 --- [           main] o.s.b.w.embedded.tomcat.TomcatWebServer  : Tomcat started on port(s): 8080 (http) with context path ''
+2018-11-24 19:27:55.198  INFO 14196 --- [           main] corp.vivek.tech.CourseApp                : Started CourseApp in 3.968 seconds (JVM running for 5.859)
+2018-11-24 19:27:55.276  INFO 14196 --- [nio-8080-exec-2] o.a.c.c.C.[Tomcat].[localhost].[/]       : Initializing Spring FrameworkServlet 'dispatcherServlet'
+2018-11-24 19:27:55.276  INFO 14196 --- [nio-8080-exec-2] o.s.web.servlet.DispatcherServlet        : FrameworkServlet 'dispatcherServlet': initialization started
+2018-11-24 19:27:55.298  INFO 14196 --- [nio-8080-exec-2] o.s.web.servlet.DispatcherServlet        : FrameworkServlet 'dispatcherServlet': initialization completed in 22 ms
 <
  */
